b3fb3d00a06488c472838f36bb639b217b6e021e
Thomas H <thomashutchinson96@gmail.com>
2018-02-12 16:01:12 +0000

    Ported Login System

---
+++ public/res/js/firebase-init.js
@@ -0,0 +1,8 @@
+import "https://www.gstatic.com/firebasejs/4.9.0/firebase.js"
+  var config = {
+    apiKey: "AIzaSyC-RyIgmlV7J1LzllCud6KQbBj3gN-omR0",
+    authDomain: "codex-8eb0b.firebaseapp.com",
+    databaseURL: "https://codex-8eb0b.firebaseio.com",
+  };
+  firebase.initializeApp(config);
+</script>
